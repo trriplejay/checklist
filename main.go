d7079244ef4b3b7276834eba70330010f8b5b9ad
John Stockbauer <stockbauer@gmail.com>
2026-06-27 14:26:35 -0700

    routing change

--- main.go
+++ main.go
@@ -11,7 +11,13 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<!DOCTYPE html><html><body><a href="/checklist">Checklist</a></body></html>`)
+	})
+	http.HandleFunc("/checklist", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "china-packing-checklist.html")
+	})
 	fmt.Println("Listening on :" + port)
 	http.ListenAndServe(":"+port, nil)
 }
